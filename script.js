e272a030640b8b589197faa4f09641183f08cbcf
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-08-28 12:09:46 +0500

    Functions:decleration,expression,arrow

---
+++ script.js
@@ -0,0 +1,32 @@
+// FUNCTION DECLARATION
+// function myFunction(num1, num2) {
+//   return num1 + num2;
+// }
+
+// console.log(myFunction(10, 0));
+// console.log(myFunction(10, 10));
+// console.log(myFunction(10, 20));
+// console.log(myFunction(20, 20));
+
+// const result = myFunction(20, 30);
+// console.log(result);
+
+// FUNCTION EXPRESSION
+// const myFunction = function (num1, num2) {
+//   return num1 + num2;
+// };
+
+// console.log(myFunction(10, 10));
+
+// ARROW FUNCTION
+// const myFunction = (num1, num2) => {
+//   return (num1 * num2) / num1;
+// };
+
+// console.log(myFunction(10, 10));
+
+// const myFunction = (num1, num2) => num1 * num2;
+// console.log(myFunction(10, 10));
+
+// const myFunction = () => console.log("MyFunction...");
+// myFunction();
